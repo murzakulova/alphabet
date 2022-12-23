880e068c64c0aea5c0b08c6fb4eeaff581c6f51b
murzakulova <aikenm310@gmail.com>
2022-12-23 13:45:39 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        FileWriter fileWriter = new FileWriter("Text.txt");
+
+    }
+}
