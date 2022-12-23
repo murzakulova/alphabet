810cd86105f5bc94aaca3c593c4b75f6bc24355d
murzakulova <aikenm310@gmail.com>
2022-12-23 13:51:12 +0600

    I add1

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,6 @@ public class Main {
     public static void main(String[] args) throws IOException {
         FileWriter fileWriter = new FileWriter("Text.txt");
         fileWriter.write("1.Aa, 2.Bb, 3.Cc, 4.Dd, 5.Ee, \n 6.Ff, 7.Gg, 8.Hh, 9.Ii, 10.Jj, 11.Kk, \n 12.Ll, 13.Mm, 14.Nn, 15.Oo, 16.Pp, \n 17.Qq, 18.Rr, 19.Ss, 20.Tt, 21.Uu, \n 22.Vv, 23.Ww, 24.Xx, 25.Yy, 26.Zz ");
-
+        fileWriter.close();
     }
 }
